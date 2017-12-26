bbfc3fc554e8338b98260d167e2e5c82cb07faee
Chaudhry Waqas <waqasraza123@gmail.com>
2017-12-27 00:22:17 +0500

    fixed searched listing design issye

--- resources/views/pages/searched_listing.blade.php
+++ resources/views/pages/searched_listing.blade.php
@@ -202,17 +202,19 @@
         <div class="col-lg-5 " id="offers-list" style="background: white;  overflow-y: scroll">
             <div class="row"  style="overflow: scroll">
                 <div class="col-xs-12">
-                    <?php function limit_text($text, $limit) {
-                        if (str_word_count($text, 0) > $limit) {
-                            $words = str_word_count($text, 2);
-                            $pos = array_keys($words);
-                            $text = substr($text, 0, $pos[$limit]) . '...';
+                    <?php
+                        function limit_text($text, $limit) {
+                            if (str_word_count($text, 0) > $limit) {
+                                $words = str_word_count($text, 2);
+                                $pos = array_keys($words);
+                                $text = substr($text, 0, $pos[$limit]) . '...';
+                            }
+                            return $text;
                         }
-                        return $text;
-                    }?>
+                    ?>
                     @foreach($listings as $listing)
                         <div class="list-agency row">
-                            <div class="list-agency-left col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                            <div class="list-agency-left col-xs-12 col-sm-6 col-md-6 col-lg-12">
                                 @if($listing->ListingsImages()->where('featured', "1")->first())
                                     <img src="{{ asset('assets/images').'/'.$listing->ListingsImages()->whereNotNull('featured')->first()->image }}" alt="" width="100%" height="250px">
                                 @endif
@@ -243,7 +245,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <a class="list-agency-right-large col-xs-12 col-sm-12 col-md-6 col-lg-6" href="{{ route('singleListing' , ['id' => $listing->listing_id , 'title' => $listing->title]) }}">
+                            <a class="list-agency-right-large col-xs-12 col-sm-6 col-md-6 col-lg-12" href="{{ route('singleListing' , ['id' => $listing->listing_id , 'title' => $listing->title]) }}">
                                 <div class="list-agency-text">
                                     <h4 class="list-agency-title"><i class="fa fa-map-marker"></i>  {{ $listing->address }}</h4>
                                     <div class="list-agency-separator"></div>
