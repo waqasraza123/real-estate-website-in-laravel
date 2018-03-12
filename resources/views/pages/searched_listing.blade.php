5d21fb2b89d5d204be5fb12b59b5bb1cade4fc3b
Artyom Petrosyan <artcas91@gmail.com>
2018-03-12 19:49:28 +0400

    MarkerCluster Map zoom

--- resources/views/pages/searched_listing.blade.php
+++ resources/views/pages/searched_listing.blade.php
@@ -832,7 +832,7 @@
             }];
 
             markerCluster = new MarkerClusterer(map, mapMarkers, {styles:markerClusterStyle});
-            minClusterZoom = 7;
+            minClusterZoom = 20;
             markerCluster.setMaxZoom(minClusterZoom);
             var oms = new OverlappingMarkerSpiderfier(map, {markersWontMove: true, markersWontHide: true, keepSpiderfied: true, legWeight: 2 });
             for (var i = 0; i < mapMarkers.length; i ++) {
